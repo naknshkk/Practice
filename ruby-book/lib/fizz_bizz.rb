fe0f64149e1e8f9d5f12c1ba4b00916f84e544ba
KOKI NAKANISHI <USER_INFO@users.noreply.github.com>
2018-11-18 22:20:48 +0900

    Commit misutta

--- ruby-book/lib/fizz_bizz.rb
+++ ruby-book/lib/fizz_bizz.rb
@@ -1,1 +1,56 @@
-puts "Hello,World"
+#puts "Hello,World"
+
+
+def fizz_bizz(n)
+  if n % 15 == 0
+    "FizzBizz"
+  elsif n % 3 == 0
+    "Fizz"
+  elsif n % 5 == 0
+    "Bizz"
+  else
+    n.to_s
+  end
+end
+
+puts fizz_bizz(1)
+puts fizz_bizz(2)
+puts fizz_bizz(3)
+puts fizz_bizz(4)
+puts fizz_bizz(5)
+puts fizz_bizz(6)
+puts fizz_bizz(7)
+puts fizz_bizz(8)
+puts fizz_bizz(9)
+puts fizz_bizz(10)
+puts fizz_bizz(11)
+puts fizz_bizz(12)
+puts fizz_bizz(13)
+puts fizz_bizz(14)
+puts fizz_bizz(15)
+puts fizz_bizz(16)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
