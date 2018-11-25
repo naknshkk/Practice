8e7be929aed9e36abdbc94217c7d74f137e8780b
KOKI NAKANISHI <USER_INFO@users.noreply.github.com>
2018-11-25 23:02:10 +0900

    ドラクエでよくある選択肢のない質問

---
+++ ruby-book/lib/DoragonQuestQuetion.rb
@@ -0,0 +1,24 @@
+foods = ['ピーマン', 'トマト', 'セロリ']
+
+count = 0
+
+foods.each do |food|
+  print "#{food}は好きですか？"
+  answer = 'いいえ'
+
+  puts answer
+
+  count += 1
+  #やり直しは2回までにする
+  redo if answer != "はい" && count < 2
+
+  count = 0
+end
+
+
+
+
+
+
+
+
