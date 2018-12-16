4cca8578c2d9529f4a09ffa2cbeea84e8b0fb88a
KOKI NAKANISHI <USER_INFO@users.noreply.github.com>
2018-12-16 14:27:47 +0900

    module Include

--- ruby-book/lib/module.rb
+++ ruby-book/lib/module.rb
@@ -1,30 +1,40 @@
+#ログ出力用のメソッドを提供するモジュール
+#「ログ出力できる（log + able）という意味でLoggableという名前をつけた
+module Loggable
+  def log(text)
+    puts "[LOG] #{text}"
+  end
+end
+
 class Product
+  include Loggable
+
   def title
     log 'title is called'
     'A great movie'
   end
 
-  private
-
-  def log(text)
-    #本来であれば標準ライブラリのLoggerクラスなどを使うべきだが、簡易的にputsで済ませる
-    puts "[LOG] #{text}"
-  end
+#  def log(text)
+#    #本来であれば標準ライブラリのLoggerクラスなどを使うべきだが、簡易的にputsで済ませる
+#    puts "[LOG] #{text}"
+#  end
 end
 
 class User
+  include Loggable
+
   def name
     log 'name is called'
     'Alice'
   end
 
-  private
+#  private
 
   #このメソッドの実装はProductクラスのLogメソッドと全く同じ
 
-  def log(text)
-    puts "[LOG] #{text}"
-  end
+#  def log(text)
+#    puts "[LOG] #{text}"
+#  end
 end
 
 product = Product.new
